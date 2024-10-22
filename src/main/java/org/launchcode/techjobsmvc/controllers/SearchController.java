c0d59bc877fbf21bb91a01c22c8df4e1f09d796e
alexdavies440 <alexdavies440@gmail.com>
2024-10-22 17:24:46 -0500

    Making progress on Task 3. Implemented title, searchType, and searchTerm modelAttributes

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobsmvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
@@ -23,5 +24,13 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+
+    @PostMapping("results")
+    public String displaySearchResults(Model model, String searchType, String searchTerm) {
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("title", "jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        return "search";
+    }
 }
 
